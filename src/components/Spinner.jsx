e3d0fb3e5d459425ce2f5aa3c384a5cbc7f175b5
Esmee29 <Esmee.Fulcher@netmatters-scs.com>
2024-05-06 09:15:34 +0100

    jobs

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,19 @@
+import ClipLoaders from 'react-spinners/ClipLoader';
+
+const override = {
+    display: 'block',
+  margin: '100px auto',
+}
+
+const Spinner = ({ loading }) => {
+  return (
+    <ClipLoaders 
+    color={'#4338ca'} 
+    loading={loading} 
+    cssOverride={override}
+    size={150} 
+    />
+  );
+};
+
+export default Spinner;
