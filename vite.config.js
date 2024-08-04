a50ae7e25ac8a1dda88881809bafd9ec45b7d387
Esmee29 <Esmee.Fulcher@netmatters-scs.com>
2024-08-04 21:34:35 +0100

    fix: some bug

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jobs-react/',
+  base: '/react-job/',
   plugins: [react()],
   server: {
     port: 3000,
